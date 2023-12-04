78862426925f7fd509a9aa462659cf3f5a5f1c39
Jennifer Winter <jennwintr@denniss-air.lan>
2023-12-04 18:39:50 -0500

    handlebars routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,28 +1,41 @@
 const router = require('express').Router();
-const Friend = require('../models/Friend');
+// const Friend = require('../models/Friend');
 
 // route to get all friends
-router.get('/', async (req, res) => {
-    const friendData = await Friend.findAll().catch((err) => { 
-        res.json(err);
-      });
-        const friends = friendData.map((friend) => friend.get({ plain: true }));
-        res.render('all', { friends });
-      });
+// router.get('/', async (req, res) => {
+//     const friendData = await Friend.findAll().catch((err) => { 
+//         res.json(err);
+//       });
+//         const friends = friendData.map((friend) => friend.get({ plain: true }));
+//         res.render('all', { friends });
+//       });
   
-  // route to get one friend
-  router.get('/friend/:id', async (req, res) => {
-    try{ 
-        const friendData = await friend.findByPk(req.params.id);
-        if(!friendData) {
-            res.status(404).json({message: 'No friend with this id!'});
-            return;
-        }
-        const friend = friendData.get({ plain: true });
-        res.render('friend', friend);
-      } catch (err) {
-          res.status(500).json(err);
-      };     
-  });
+//   // route to get one friend
+//   router.get('/friend/:id', async (req, res) => {
+//     try{ 
+//         const friendData = await friend.findByPk(req.params.id);
+//         if(!friendData) {
+//             res.status(404).json({message: 'No friend with this id!'});
+//             return;
+//         }
+//         const friend = friendData.get({ plain: true });
+//         res.render('friend', friend);
+//       } catch (err) {
+//           res.status(500).json(err);
+//       };     
+//   });
+
+router.get('/', async (req, res) => {
+  res.render("homepage")
+});
+
+router.get('/profile', async (req, res) => {
+  res.render("profile")
+});
+
+router.get('/sign-up', async (req, res) => {
+  res.render("sign-up")
+});
 
 module.exports = router;
+
