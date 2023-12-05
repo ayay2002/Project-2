7177234a9f0b997c90759ece13bcf5b1c2936047
Wolffkran <liebherr2008@live.com>
2023-12-04 19:21:09 -0500

    Added profile.js, built framework for homepage and profile handlebars

---
+++ profile.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('controllers\api\profile')
+      .then(response => response.json())
+      .then(profileData => {
+        updateProfileUI(profileData);
+      })
+      .catch(error => {
+        console.error('Error fetching profile data:', error);
+      });
+  
+    /* Function to update the UI with profile data */
+    function updateProfileUI(profileData) {
+      /* Assuming you have elements in your HTML to display the profile data */
+      const usernameElement = document.getElementById('username');
+      const emailElement = document.getElementById('email');
+      const bioElement = document.getElementById('bio');
+  
+      /* Update the elements with the fetched data */
+      usernameElement.textContent = profileData.username;
+      emailElement.textContent = profileData.email;
+      bioElement.textContent = profileData.bio;
+    }
+  });
+  
