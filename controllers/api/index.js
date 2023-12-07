53ea8f3d4ce10ab1002efbc70817b854ce7424b8
Alexander <alexandercryptock@icloud.com>
2023-12-06 20:28:03 -0500

    s

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const friendRoutes = require('./friend-routes');
+const userRoutes = require('./user-routes');
 
 router.use('/friend', friendRoutes);
+router.use('/user', userRoutes);
 
 module.exports = router;
