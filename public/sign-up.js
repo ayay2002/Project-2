b25839fcad978256d42e808d9a69358be3687161
Ruminate <inramiland@gmail.com>
2023-12-06 21:09:41 -0500

    updated signup.js

--- public/sign-up.js
+++ public/sign-up.js
@@ -1,51 +1,48 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const signUpForm = document.getElementById('signup-form');
-  
-    signUpForm.addEventListener('submit', async (event) => {
-      event.preventDefault();
-  
-      // Fetch input values
-      const username = document.getElementById('username').value;
-      const email = document.getElementById('email').value;
-      const password = document.getElementById('password').value;
-      const confirmPassword = document.getElementById('confirm-password').value;
-  
-      // Validate input (you can add more validation as needed)
-      if (!username || !email || !password || password !== confirmPassword) {
-        alert('Please fill in all fields and ensure passwords match.');
-        return;
+  const signUpForm = document.getElementById('signup-form');
+
+  signUpForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    // Extract form data
+    const username = document.getElementById('username').value;
+    const firstName = document.getElementById('first_name').value;
+    const lastName = document.getElementById('last_name').value;
+    const age = document.getElementById('age').value;
+    const race = document.getElementById('race').value;
+    const birthCity = document.getElementById('birth_city').value;
+    const homeCity = document.getElementById('home_city').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    try {
+      // Send POST request to server with all form data
+      const response = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username,
+          first_name: firstName,
+          last_name: lastName,
+          age: age,
+          race: race,
+          birthCity: birth_city,
+          homeCity: home_city,
+          email: email,
+          password: password,
+
+        }),
+      });
+
+      if (response.ok) {
+        // need to update to handle successful sign up
+      } else {
+        // need to update to handle sign up error
       }
-  
-      // Create user object
-      const user = {
-        username,
-        email,
-        password,
-      };
-  
-      try {
-        // Make a POST request to your signup API endpoint
-        const response = await fetch('/api/signup', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(user),
-        });
-  
-        if (response.ok) {
-          // Optionally, redirect to another page or show a success message
-          console.log('User signed up successfully!');
-        } else {
-          // Handle error response
-          const errorData = await response.json();
-          console.error('Signup failed:', errorData.message);
-          alert('Signup failed. Please try again.');
-        }
-      } catch (error) {
-        console.error('Error during signup:', error);
-        alert('An error occurred. Please try again.');
-      }
-    });
+    } catch (error) {
+      console.error('An error occurred during sign-up:', error);
+    }
   });
-  
+});
