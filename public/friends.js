46ecf496371412f4723dedbf4ab1685aa6500928
Wolffkran <liebherr2008@live.com>
2023-12-14 11:46:34 -0500

    Added functionality to friends list and seeded it

--- public/friends.js
+++ public/friends.js
@@ -1,32 +1,86 @@
-// friends.js
-
 document.addEventListener('DOMContentLoaded', () => {
-    fetchFriendsData();
+    seedDatabaseAndDisplayFriends();
 });
 
-function fetchFriendsData() {
-    fetch('../seeds/friends-seeds.json')
-        .then(response => response.json())
+function seedDatabaseAndDisplayFriends() {
+    seedDatabase()
         .then(data => {
             displayFriends(data);
             setupEventListeners(data);
         })
-        .catch(error => console.error('Error fetching data:', error));
+        .catch(error => console.error('Error seeding database or displaying friends:', error));
 }
 
-fetchFriendsData();
+function seedDatabaseAndDisplayFriends() {
+    seedDatabase()
+        .then(data => {
+            displayFriends(data);
+        })
+        .catch(error => console.error('Error seeding database or displaying friends:', error));
+}
+
+function seedDatabase() {
+    const friendsData = [
+        {
+            "first_name": "John",
+            "last_name": "Smith",
+            "age": "26",
+            "race": "white",
+            "birth_city": "Charleston",
+            "home_city": "Richmond"
+        },
+        {
+            "first_name": "Devonte",
+            "last_name": "Robinson",
+            "age": "32",
+            "race": "Black",
+            "birth_city": "New Orleans",
+            "home_city": "Baton Rouge"
+        },
+        {
+            "first_name": "Cristiano",
+            "last_name": "Ronoldo",
+            "age": "37",
+            "race": "Hispanic",
+            "birth_city": "Miami",
+            "home_city": "Orlando"
+        },
+        {
+            "first_name": "Chang",
+            "last_name": "Wong",
+            "age": "76",
+            "race": "Asian",
+            "birth_city": "Houston",
+            "home_city": "Dallas"
+        },
+        {
+            "first_name": "Aleut",
+            "last_name": "Cupan",
+            "age": "17",
+            "race": "Alaskan Native",
+            "birth_city": "Anchorage",
+            "home_city": "Seattle"
+        }
+    ];
+
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(friendsData);
+        }, 1000);
+    });
+}
 
 function displayFriends(friends) {
     const friendsListContainer = document.getElementById('friendsList');
     friends.forEach(friend => {
         const listItem = document.createElement('li');
         listItem.className = 'list-group-item d-flex justify-content-between align-items-center';
-        listItem.textContent = friend.first_name + ' ' + friend.Last_name;
+        listItem.textContent = friend.first_name + ' ' + friend.last_name;
 
         const deleteButton = document.createElement('button');
         deleteButton.className = 'btn btn-danger btn-sm';
         deleteButton.textContent = 'Delete';
-        deleteButton.onclick = () => deleteFriend(friend.id, listItem);
+        deleteButton.onclick = () => deleteFriend(friend.first_name, listItem);
 
         listItem.appendChild(deleteButton);
         friendsListContainer.appendChild(listItem);
@@ -38,15 +92,12 @@ function setupEventListeners(allFriends) {
 }
 
 function searchForFriend(allFriends) {
-    // Get the search query from the input field
     const searchQuery = document.getElementById('searchInput').value;
 
-    // Filter friends based on the search query
     const matchingFriends = allFriends.filter(friend =>
-        (friend.first_name + ' ' + friend.Last_name).toLowerCase().includes(searchQuery.toLowerCase())
+        (friend.first_name + ' ' + friend.last_name).toLowerCase().includes(searchQuery.toLowerCase())
     );
 
-    // Display the search results
     const searchResultsContainer = document.getElementById('searchResults');
     searchResultsContainer.innerHTML = '';
 
@@ -56,48 +107,39 @@ function searchForFriend(allFriends) {
         const resultList = document.createElement('ul');
         matchingFriends.forEach(friend => {
             const listItem = document.createElement('li');
-            listItem.textContent = friend.first_name + ' ' + friend.Last_name;
+            listItem.textContent = friend.first_name + ' ' + friend.last_name;
             resultList.appendChild(listItem);
         });
         searchResultsContainer.appendChild(resultList);
     }
 }
 
-function deleteFriend(friendId, listItem) {
-    // Alert to notify the user before deleting
-    const confirmDelete = window.confirm(`Are you sure you want to delete this friend with ID ${friendId}?`);
+function deleteFriend(friendName, listItem) {
+    const confirmDelete = window.confirm(`Are you sure you want to delete this friend ${friendName}?`);
 
     if (!confirmDelete) {
         return;
     }
 
-    // Assuming you have an API endpoint for deleting a friend
-    const deleteEndpoint = `/api/friends/${friendId}`;
+    const deleteEndpoint = `/api/friends/${encodeURIComponent(friendName)}`;
 
-    // You can use the Fetch API to make an HTTP DELETE request
     fetch(deleteEndpoint, {
         method: 'DELETE',
         headers: {
             'Content-Type': 'application/json',
-            // Include any additional headers as needed
         },
-        // You can include a request body if needed
-        // body: JSON.stringify({ friendId: friendId }),
     })
     .then(response => {
         if (!response.ok) {
-            throw new Error(`Failed to delete friend with ID ${friendId}`);
+            throw new Error(`Failed to delete friend ${friendName}`);
         }
         return response.json();
     })
     .then(data => {
-        // Handle success, if needed
-        console.log(`Friend with ID ${friendId} deleted successfully`);
-        // You might want to refresh the friends list or update the UI accordingly
-        listItem.remove(); // Remove the deleted friend from the UI
+        console.log(`Friend ${friendName} deleted successfully`);
+        listItem.remove();
     })
     .catch(error => {
-        // Handle errors, if needed
         console.error('Error deleting friend:', error);
     });
 }
