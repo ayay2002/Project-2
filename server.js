519eb538bec88ffdc20c506452982da91ccab027
Wolffkran <liebherr2008@live.com>
2023-12-13 21:25:15 -0500

    Restructured server.js

--- server.js
+++ server.js
@@ -6,26 +6,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const { Sequelize } = require('sequelize');
 
-let sequelize;
+const sequelize = require('./config/connection')
 
-
-
-if (process.env.JAWSDB_URL) {
-  // Use Heroku database
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  // Use local database configuration
-  sequelize = new Sequelize({
-    host: 'localhost',  // Update with your local host
-    username: 'root', // Update with your local username
-    password: 'Hr3694642', // Update with your local password
-    port: 3306,
-    database: 'friends_db', // Update with your local database
-    dialect: 'mysql',
-  });
-}
 
 const sess = {
   secret: 'Super secret secret',
@@ -55,6 +38,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers'));
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log('Now listening on port ' + PORT));
 });
