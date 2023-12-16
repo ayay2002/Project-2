97fc5cc366682b98f51999779655642db4454764
Alexander <alexandercryptock@icloud.com>
2023-12-16 15:21:49 -0500

    g

--- server.js
+++ server.js
@@ -13,20 +13,20 @@ const sequelize = require('./config/connection')
 
 
 
-// if (process.env.JAWSDB_URL) {
-//   // Use Heroku database
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//   // Use local database configuration
-//   sequelize = new Sequelize({
-//     host: 'localhost',  // Update with your local host
-//     username: 'root', // Update with your local username
-//     password: 'Hr3694642', // Update with your local password
-//     port: 3306,
-//     database: 'friends_db', // Update with your local database
-//     dialect: 'mysql',
-//   });
-// }
+if (process.env.JAWSDB_URL) {
+  // Use Heroku database
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  // Use local database configuration
+  sequelize = new Sequelize({
+    host: 'localhost',  // Update with your local host
+    username: 'root', // Update with your local username
+    password: 'Hr3694642', // Update with your local password
+    port: 3306,
+    database: 'friends_db', // Update with your local database
+    dialect: 'mysql',
+  });
+}
 
 const sess = {
   secret: 'Super secret secret',
